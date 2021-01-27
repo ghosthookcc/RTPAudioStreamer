497aa986913975d87868855bc53ca76e65ce9e1e
ghosthookcc <kasperthornsten@gmail.com>
2021-01-27 17:19:55 +0100

    Replaced struct with class for optimisation purposes

--- RTPAudio/RTPAudio.cs
+++ RTPAudio/RTPAudio.cs
@@ -24,25 +24,36 @@ namespace file_splitter
    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 
     */
-    public struct PacketBuilderParam
+    public class PacketBuilderParam
     {
-        Random rand => new Random();
-        public UInt32 timestamp => Convert.ToUInt32(rand.Next(999, 43576));
-        public UInt16 sequence => Convert.ToUInt16(rand.Next(1000, 22545));
+        public UInt32 timestamp;
+        public UInt16 sequence;
         public UInt16 frag_off;
-        public int dataperpacket => 500; //size of the payload. 
+        public int dataperpacket; //size of the payload. 
         public byte byte1;//has rtp_version, rtp_padding, rtp_extension, rtp_csources_count;
         public byte byte2; // has  rtp_marker, rtp_payload_type;
-        public int rtp_identifier => rand.Next(1001, 1000000);
+        public int rtp_identifier;
         public UInt32 rtp_csources;
         public UInt32 rtp_ssrc;
         public UInt32 rtp_csourcecount;
 
+        public PacketBuilderParam()
+        {
+            Random rand = new Random();
+
+            timestamp = Convert.ToUInt32(rand.Next(999, 43576));
+            sequence = Convert.ToUInt16(rand.Next(1000, 22545));
+            rtp_identifier = rand.Next(1001, 1000000);
+
+            dataperpacket = 500;
+            frag_off = 0;
+        }
     }
 
 
     public class PacketBuilder
     {
+        PacketBuilderParam packetinfo = new PacketBuilderParam();
 
         string selectedfile;
         public Queue<byte[]> splitfiledata;
@@ -50,8 +61,6 @@ namespace file_splitter
         public Queue<byte[]> packets;
         public byte[] DEBUG_header;
         public byte[] DEBUG_packet;
-        PacketBuilderParam packetinfo = new PacketBuilderParam();
-
 
         int dataperpacket => packetinfo.dataperpacket;
         public PacketBuilder(string filepath)
@@ -62,7 +71,6 @@ namespace file_splitter
         }
         Queue<byte[]> BuildPayload(string path)
         {
-
             Queue<byte[]> filepackets = new Queue<byte[]>();
             using (FileStream file = File.OpenRead(path))
             {
@@ -97,9 +105,7 @@ namespace file_splitter
 
         Queue<byte[]> BuildPacket(Queue<byte[]> payload) //This whole method needs to be refactored ASAP. Suggested: pointers instead of bitconversion, buffer usage instead of whatever kind of bullshit we used to manipulate the arrays.
         {
-            packetinfo.frag_off = 0;
             int remainingpackets = payload.Count;
-
 
             byte[] sequencebytes = new byte[2];
             byte[] fragoffset = new byte[2];
@@ -107,17 +113,11 @@ namespace file_splitter
 
             Queue<byte[]> packetlist = new Queue<byte[]>();
 
-            UInt16 sequence = packetinfo.sequence; // we shouldnt need to do this, fix this!
-            UInt32 timestamp = packetinfo.timestamp; //Ditto.
-
-
-            timestampbytes = BitConverter.GetBytes(timestamp);
+            timestampbytes = BitConverter.GetBytes(packetinfo.timestamp);
             Array.Reverse(timestampbytes, 0, 4);
 
             packetinfo.byte1 = 0b_10_0_0_0001;
             packetinfo.byte2 = 0b_0_0_001110;
-
-
 
             byte[] header = new byte[20];
             header[0] = packetinfo.byte1;
@@ -131,10 +131,10 @@ namespace file_splitter
                 fragoffset = BitConverter.GetBytes(packetinfo.frag_off);
                 Array.Reverse(fragoffset);
 
-                sequencebytes = BitConverter.GetBytes(sequence);
+                sequencebytes = BitConverter.GetBytes(packetinfo.sequence);
                 Array.Reverse(sequencebytes, 0, 2);
 
-                timestampbytes = BitConverter.GetBytes(timestamp);
+                timestampbytes = BitConverter.GetBytes(packetinfo.timestamp);
                 Array.Reverse(timestampbytes, 0, 4);
 
                 Array.ConstrainedCopy(sequencebytes, 0, header, 2, 2);
@@ -146,9 +146,9 @@ namespace file_splitter
                 Array.ConstrainedCopy(payload.Dequeue(), 0, packet, 20, dataperpacket);
 
                 packetlist.Enqueue(packet);
-                sequence++;
-                timestamp += 80; // 80 works for some reason, I still to this day have no clue why
-                packetinfo.frag_off += 0;
+
+                packetinfo.sequence++;
+                packetinfo.timestamp += 80; // 80 works for some reason, I still to this day have no clue why
             }
 
             return packetlist;
@@ -159,7 +159,8 @@ namespace file_splitter
         static Queue<byte[]>[] filestostream;
         static void Main()
         {
-            PacketBuilder packets = new PacketBuilder(@"C:\Users\lasse\source\repos\RTPTesting\AetherRealmSunMoonStar.mp3");
+            //  PacketBuilder packets = new PacketBuilder(@"pathtofile\RTPAudio\UnlikePlutoEverythingBlack.mp3");
+            PacketBuilder packets = new PacketBuilder(@"C:\Users\lasse\source\repos\RTPAudioStreamer-main\RTPAudio\UnlikePlutoEverythingBlack.mp3");
 
             EndPoint RemoteEP = new IPEndPoint(IPAddress.Parse("0.0.0.0"), 8079);
             EndPoint SendtoEP = new IPEndPoint(IPAddress.Parse("192.168.1.107"), 8080);
@@ -173,7 +174,7 @@ namespace file_splitter
             int remainingpackets = DEBUG_packets.Count;
             for (int x = 0; x < remainingpackets; x++)
             {
-                Console.WriteLine("Packet {0} sent.", x);
+                // Console.WriteLine("Packet {0} sent.", x);
                 sender.SendTo(DEBUG_packets.Dequeue(), SendtoEP);
             }
         }
